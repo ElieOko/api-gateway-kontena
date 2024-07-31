d5104b59b5ceb229d1d0769840cf00a0e4f1e22b
ElieOko <elieoko100@gmail.com>
2024-07-31 10:07:07 +0100

    fix:constante user

---
+++ src/utils/constant/user.ts
@@ -0,0 +1,10 @@
+import { IUser } from "../interface/user/IUser";
+
+export const user : IUser = {
+    user_id : 1,
+    username : "ElieOko",
+    password : "opensource243#KC",
+    email : "elieoko100@gmail.com",
+    is_active : true,
+    token : [{name:"", service:"x"},{name:"x", service:"y"}]
+}
