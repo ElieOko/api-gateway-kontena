f6cc187a1096c4b26c9c481859fdf1c4c1110a93
ElieOko <elieoko100@gmail.com>
2024-08-02 05:21:08 +0100

    fix:config global all worker in typescript

---
+++ src/config/worker/worker.ts
@@ -0,0 +1,28 @@
+import {
+    Worker,
+    getEnvironmentData,
+    isMainThread,
+    parentPort,
+    workerData,
+    WorkerOptions
+} from "worker_threads";
+
+//
+// WORKAROUND (GAMBIARRA)
+//
+export const workerTs = (file: string, wkOpts: WorkerOptions) => {
+    wkOpts.eval = true;
+    if (!wkOpts.workerData) {
+        wkOpts.workerData = {};
+    }
+    wkOpts.workerData.__filename = file;
+    return new Worker(`
+            const wk = require('worker_threads');
+            require('ts-node').register();
+            let file = wk.workerData.__filename;
+            delete wk.workerData.__filename;
+            require(file);
+        `,
+        wkOpts
+    );
+}
