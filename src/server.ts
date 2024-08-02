972eac2a4bb345a87114569733b6a56e077dc2d5
ElieOko <elieoko100@gmail.com>
2024-08-02 05:29:27 +0100

    fix:merge cluster and worker with choice cpu executor

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,48 @@
 import express from "express";
 import { mongo } from "./database/mongo";
 import { create } from "./controller/user/model";
-import { user } from "./utils/constant/user";
+import { user, userExtend } from "./utils/constant/user";
 import cluster from "cluster";
-
+import { Worker, getEnvironmentData, isMainThread, setEnvironmentData, workerData } from "worker_threads";
+import { serviceAchat } from "./thread/worker/worker_achat";
+import helmet from "helmet";
+import morgan from "morgan";
+import * as os from 'os';
 
 const app = express();
-const port = process.env.PORT || 3000;
+let clusterCpuRun = false;
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(helmet());
+const port = process.env.PORT || 5000;
+const numCPUs = os.cpus().length;
+if (app.get("env")==="development") {
+    app.use(morgan('tiny'));
+}
+setEnvironmentData('user',userExtend)
 if(cluster.isPrimary){
-    console.log("master")
-    cluster.fork()
-    cluster.fork()
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died`);
+    });
+
+} else{
+    if(cluster?.worker?.id === 1){
+        if(!clusterCpuRun){ 
+            serviceAchat();
+            clusterCpuRun = true
+        }
+        app.listen(port,()=> {
+            console.log(`Server launch ${port}...`)
+        });
+    }
+      
 }
-else{
+// if(isMainThread){
+   
+// } 
 
-    app.get("/test",(req,res)=>{
-    res.send("Hello Bro");
-     console.log("Hi dev");
-     //create(user)
-
-     })
-
-    app.listen(port,()=> {console.log("child started")});
-}
-// if (app.get("env")==="development") {
-//     // app.use(morgan('tiny'));
-// }
-
-
-
-// app.listen(port,()=> {
-//     console.log(`Server launch ${port}...`)
-//     console.log(`worker pid=${process.pid}`)
-// });
